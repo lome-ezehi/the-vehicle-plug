1671a86a32e25b8cf76698c14014de1b72c02f5d
Veritas <165820985+lome-ezehi@users.noreply.github.com>
2025-07-05 17:14:00 +0100

    Added Landing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,45 +5,50 @@ import '../assets/styles/Navbar.css'
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-            <a className="navbar-brand fst-italic" href="#">TheCarDealers</a>
+            <div className="container-fluid">
+                <Link className="navbar-brand fst-italic" to="/">TheCarDealers</Link>
+                
                 <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarText"
-                    aria-controls="navbarText"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarContent"
+                aria-controls="navbarContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
                 >
                 <span className="navbar-toggler-icon"></span>
-            </button>
+                </button>
 
-            <div className="collapse navbar-collapse" id="navbarText">
+                <div className="collapse navbar-collapse" id="navbarContent">
                 <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                     <li className="nav-item">
                     <NavLink 
-                    to="/" 
-                    className={({ isActive }) =>
-                        isActive ? "active nav-link" : "nav-link"
-                    }
+                        to="/" 
+                        className={({ isActive }) =>
+                        isActive ? "nav-link active" : "nav-link"
+                        }
                     >Home</NavLink>
                     </li>
                     <li className="nav-item">
-                    <NavLink to="/cars" className="nav-link">Cars</NavLink>
+                    <NavLink to="/inventory" className="nav-link">Inventory</NavLink>
                     </li>
                     <li className="nav-item">
-                    <NavLink to="/categories" className="nav-link">Categories</NavLink>
+                    <NavLink to="/financing" className="nav-link">Financing</NavLink>
                     </li>
                     <li className="nav-item">
-                    <NavLink to="/about" className="nav-link">About</NavLink>
+                    <NavLink to="/services" className="nav-link">Services</NavLink>
+                    </li>
+                    <li className="nav-item">
+                    <NavLink to="/contact" className="nav-link">Contact</NavLink>
                     </li>
                 </ul>
-                <Link className="CTA">
-                    Order Now
+
+                <Link to="/order" className="btn btn-primary">
+                    Book a Test Drive
                 </Link>
+                </div>
             </div>
-        </div>
         </nav>
     )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import landingAudi from '../assets/images/audi-landing.png'
+import amg from '../assets/images/amgc63.png'
 
 export default function Home() {
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            {/* Main */}
+                <div className="d-flex flex-row align-items-center justify-content-around">
+                    <h1 className="display-5 fw-bold text-center">Your Number <br /> 1 Car Plug</h1>
+                    <img
+                        src={landingAudi}
+                        alt="Audi car on display"
+                        className="img-fluid"
+                        style={{ width: '800px', height: 'auto' }}
+                    />
+                </div>
+        </>
     )   
 }
 
