574d981c05a850f19fdd8df1495ae785516d0033
lome-ezehi <lomeezehi@gmail.com>
2025-07-10 08:44:29 +0100

    Fixed Bugs

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,45 @@
 import { useState } from "react";
 import { db } from "./firebase";
 import { collection, addDoc, getDocs } from "firebase/firestore";
+import Home from "./pages/index"
+import { Route, Routes } from "react-router-dom";
+import React from "react";
 
-type User = {
-  name: string;
-  age: number;
-};
+// type User = {
+//   name: string;
+//   age: number;
+// };
 
-function App() {
-  const [users, setUsers] = useState<User[]>([]);
-
-  const handleSave = async () => {
-    await addDoc(collection(db, "users"), { name: "Alice", age: 25 });
-  };
-
-  const handleLoad = async () => {
-    const querySnapshot = await getDocs(collection(db, "users"));
-    const fetchedUsers: User[] = [];
-    querySnapshot.forEach((doc) => {
-      fetchedUsers.push(doc.data() as User);
-    });
-    setUsers(fetchedUsers);
-  };
-
+export default function App() {
   return (
-    <div>
-      <button onClick={handleSave}>Save Data</button>
-      <button onClick={handleLoad}>Load Data</button>
-      <ul>
-        {users.map((u, i) => (
-          <li key={i}>{u.name} - {u.age}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
 }
+  // const [users, setUsers] = useState<User[]>([]);
 
-export default App;
+  // const handleSave = async () => {
+  //   await addDoc(collection(db, "users"), { name: "Alice", age: 25 });
+  // };
+
+  // const handleLoad = async () => {
+  //   const querySnapshot = await getDocs(collection(db, "users"));
+  //   const fetchedUsers: User[] = [];
+  //   querySnapshot.forEach((doc) => {
+  //     fetchedUsers.push(doc.data() as User);
+  //   });
+  //   setUsers(fetchedUsers);
+  // };
+
+  // return (
+  //   <div>
+  //     <button onClick={handleSave}>Save Data</button>
+  //     <button onClick={handleLoad}>Load Data</button>
+  //     <ul>
+  //       {users.map((u, i) => (
+  //         <li key={i}>{i} {u.name} - {u.age}</li>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // );
